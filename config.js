72ad53b2e2423a0e57be3db6da9b7e9c18448346
DominicDev <s0l0estudi4r@gmail.com>
2026-03-13 18:08:25 +0100

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'RIHANNA';
+export const nameSongs = [
+	"UMBRELLA", /*1*/
+	"WE FOUND LOVE ", /*2*/
+	"DIAMONDS", /*3*/
+	"ONLY GIRL", /*4*/
+	"RUDE BOY", /*5*/
+	"WORK", /*6*/
+	"DISTURBIA", /*7*/
+	"DON'T STOP THE MUSIC", /*8*/
+	"LOVE THE WAY YOU LIE ", /*9*/
+	"WHAT'S MY NAME? ", /*10*/
+	"TAKE A BOW", /*11*/
+	"SOS", /*12*/
+	"WHERE HAVE YOU BEEN", /*13*/
+	"PON DE REPLAY", /*14*/
+	"MAN DOWN", /*15*/
+	"STAY ", /*16*/
+	"TE AMO", /*17*/
+	"UNFAITHFUL", /*18*/
+	"SHUT UP AND DRIVE", /*19*/
+	"KISS IT BETTER", /*20*/
+]
